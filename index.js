9f743e8c5bd54076e7ae8f24e422850b3c098ed8
Dara Adenipekun <daraadenipekun97@gmail.com>
2022-01-20 11:02:27 +0100

    add the utility folder and imported functions

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const {cut3 , largeNumber} = require("./utilities/util1")
+const {sum, concat} =require("./utilities/util2");
+
+
+ console.log(cut3([1,2,3,4,5]));
+
+console.log(largeNumber([1,2,3]))
+
+
+console.log(sum([10,20,30,40]));
+
+console.log(concat([5,6,7], [10,9,8]));
 
